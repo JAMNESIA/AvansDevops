ee2d9fc6320acb43a397a6623b6f0473bca2fd37
Jamie Surowiec <jamiesurowiec@hotmail.com>
2023-03-18 20:18:31 +0100

    Remove unused code

--- src/report/Footer.ts
+++ src/report/Footer.ts
@@ -7,22 +7,6 @@ export class Footer{
         this._reportDate = new Date();
     }
 
-    public getProjectVersion(): string{
-        return this._projectVersion;
-    }
-
-    public getReportDate(): Date{
-        return this._reportDate;
-    }
-
-    public setProjectVersion(projectVersion: string): void{
-        this._projectVersion = projectVersion;
-    }
-
-    public setReportDate(reportDate: Date): void{
-        this._reportDate = reportDate;
-    }
-
     public toString(): string{
         return "\nReport generated on: " + this._reportDate.toISOString() + " for version: " + this._projectVersion;
     }
--- src/report/Header.ts
+++ src/report/Header.ts
@@ -9,30 +9,6 @@ export class Header{
         this._reportTitle = reportTitle;
     }
 
-    public getCompanyName(): string{
-        return this._companyName;
-    }
-
-    public getProjectTitle(): string{
-        return this._projectTitle;
-    }
-
-    public getReportTitle(): string{
-        return this._reportTitle;
-    }
-
-    public setCompanyName(companyName: string): void{
-        this._companyName = companyName;
-    }
-
-    public setProjectTitle(projectTitle: string): void{
-        this._projectTitle = projectTitle;
-    }
-
-    public setReportTitle(reportTitle: string): void{
-        this._reportTitle = reportTitle;
-    }
-
     public toString(): string{
         return "Company: " + this._companyName + " Project: " + this._projectTitle + " Report: " + this._reportTitle + "\n";
     }
