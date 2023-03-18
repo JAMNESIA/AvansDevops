d6f975f61275181fcb1af38feea37c1b03c6607e
Jamie Surowiec <jamiesurowiec@hotmail.com>
2023-03-18 21:05:07 +0100

    clear main for code coverage

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import { ConcreteReportBuilder } from "./report/ConcreteReportBuilder";
-
-const reportBuilder = new ConcreteReportBuilder();
-reportBuilder.produceHeader("Avans", "Avans DevOps", "Sprint 1").produceBody("Sprint 1 150 story points were done.").produceBody("Team members: Jamie Surowiec & Stan Barkmeijer.").produceFooter("1.0.0").producePDF().producePNG();
