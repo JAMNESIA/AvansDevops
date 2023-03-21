159b10e657412af702e1a07e3aa0c832f4c04e92
Stan Barkmeijer <stan@barkmeijer.com>
2023-03-21 09:57:42 +0100

    Forum

---
+++ src/forum/CommentTree.ts
@@ -0,0 +1,54 @@
+import { Comment } from './Comment';
+import { CommentNode } from './CommentNode';
+
+export class CommentTree {
+
+    private _root: CommentNode;
+
+    constructor(root: CommentNode) {
+        this._root = root;
+    }
+
+    public getRoot(): CommentNode {
+        return this._root;
+    }
+
+    public log(): void {
+        this._root.log();
+    }
+
+    public addComment(comment: Comment): void {
+        let node = new CommentNode(comment);
+
+        this._root.addChild(node);
+    }
+
+    public removeComment(comment: Comment): void {
+        let node = this.findNode(comment);
+
+        if (node) {
+            node.getParent().removeChild(node);
+        }
+    }
+
+    public findNode(comment: Comment): CommentNode {
+        return this.findNodeRecursive(this._root, comment);
+    }
+
+    private findNodeRecursive(node: CommentNode, comment: Comment): CommentNode {
+        if (node.getComment().getId() === comment.getId()) {
+            return node;
+        }
+
+        for (let child of node.getChildren()) {
+            let result = this.findNodeRecursive(child, comment);
+
+            if (result) {
+                return result;
+            }
+        }
+
+        return null;
+    }
+
+}
