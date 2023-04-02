d8e89888df87dbf4fe5eee374e1db41e4ec60567
Jamie Surowiec <jamiesurowiec@hotmail.com>
2023-04-02 15:41:49 +0200

    add receipent

--- tests/notification/Notification.test.ts
+++ tests/notification/Notification.test.ts
@@ -6,27 +6,27 @@ import { expect } from "chai";
 
 describe('Notification', () => {
     it('should send a message', () => {
-        const notifier = new ConcreteNotifier();
-        expect(notifier.notify('Hello')).to.equal('Hello');
+        const notifier = new ConcreteNotifier("John");
+        expect(notifier.notify('Hello')).to.equal('Message for John: Hello');
     });
     
     it('should send a message via email', () => {
-        const notifier = new MailNotifier(new ConcreteNotifier());
-        expect(notifier.notify('Hello')).to.equal('Sending email message: Hello');
+        const notifier = new MailNotifier(new ConcreteNotifier("John"));
+        expect(notifier.notify('Hello')).to.equal('Sending email message: Message for John: Hello');
     }); 
 
     it('should send a message via slack', () => {
-        const notifier = new SlackNotifier(new ConcreteNotifier());
-        expect(notifier.notify('Hello')).to.equal('Sending Slack message: Hello');
+        const notifier = new SlackNotifier(new ConcreteNotifier("John"));
+        expect(notifier.notify('Hello')).to.equal('Sending Slack message: Message for John: Hello');
     });
 
     it('should send a message via email and slack', () => {
-        const notifier = new SlackNotifier(new MailNotifier(new ConcreteNotifier()));
-        expect(notifier.notify('Hello')).to.equal('Sending Slack message: Sending email message: Hello');
+        const notifier = new SlackNotifier(new MailNotifier(new ConcreteNotifier("John")));
+        expect(notifier.notify('Hello')).to.equal('Sending Slack message: Sending email message: Message for John: Hello');
     });
 
     it('should send a message via slack and email', () => {
-        const notifier = new MailNotifier(new SlackNotifier(new ConcreteNotifier()));
-        expect(notifier.notify('Hello')).to.equal('Sending email message: Sending Slack message: Hello');
+        const notifier = new MailNotifier(new SlackNotifier(new ConcreteNotifier("John")));
+        expect(notifier.notify('Hello')).to.equal('Sending email message: Sending Slack message: Message for John: Hello');
     });
 });
